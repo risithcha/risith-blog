ff2bd38957dc8c7ebeceaa8f716f41dfd4f6f167
risithcha <risithcha@gmail.com>
2025-08-26 14:46:55 -0700

    Ahhhhhhhh! The links on the footer were wrong!

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,8 +5,8 @@ export default function Footer() {
       <div className="max-w-4xl mx-auto px-6 py-8">
         {/* Gotta advertise my self somehow... */}
         <div className="flex justify-center space-x-8 mb-4 text-sm text-gray-400">
-          <a href="https://github.com" className="hover:text-white" target="_blank" rel="noopener noreferrer">GitHub</a>
-          <a href="https://linkedin.com" className="hover:text-white" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+          <a href="https://github.com/risithcha" className="hover:text-white" target="_blank" rel="noopener noreferrer">GitHub</a>
+          <a href="https://linkedin.com/in/risithcha/" className="hover:text-white" target="_blank" rel="noopener noreferrer">LinkedIn</a>
           <a href="mailto:risitha@gmail.com" className="hover:text-white">Email</a>
         </div>
         {/* "Copyright" stuff */}
